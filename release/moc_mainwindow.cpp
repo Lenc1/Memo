7965bdea8079423af6a73bae3009f41e45bad702
Lenci <38912035+Lenc1@users.noreply.github.com>
2023-03-31 20:38:35 +0800

    minor modifications

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[89];
+    QByteArrayData data[7];
+    char stringdata0[107];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,12 +36,13 @@ QT_MOC_LITERAL(1, 11, 15), // "showChildDialog"
 QT_MOC_LITERAL(2, 27, 0), // ""
 QT_MOC_LITERAL(3, 28, 18), // "on_newmemo_clicked"
 QT_MOC_LITERAL(4, 47, 19), // "on_viewmemo_clicked"
-QT_MOC_LITERAL(5, 67, 21) // "on_backpackbt_clicked"
+QT_MOC_LITERAL(5, 67, 21), // "on_backpackbt_clicked"
+QT_MOC_LITERAL(6, 89, 17) // "on_browse_clicked"
 
     },
     "MainWindow\0showChildDialog\0\0"
     "on_newmemo_clicked\0on_viewmemo_clicked\0"
-    "on_backpackbt_clicked"
+    "on_backpackbt_clicked\0on_browse_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +52,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,12 +60,14 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x0a /* Public */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x0a /* Public */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    0,   42,    2, 0x08 /* Private */,
+       6,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -83,6 +86,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 1: _t->on_newmemo_clicked(); break;
         case 2: _t->on_viewmemo_clicked(); break;
         case 3: _t->on_backpackbt_clicked(); break;
+        case 4: _t->on_browse_clicked(); break;
         default: ;
         }
     }
@@ -114,13 +118,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 5;
     }
     return _id;
 }
